a39a8614b1d5785837c28b508e716ac5b14afed8
Sharon <seaustin@live.unc.edu>
2014-03-28 09:56:40 -0400

    updates to survey readout script, done with a tutorial

--- readSurvey.py
+++ readSurvey.py
@@ -1,3 +1,18 @@
 import csv
+surveyFile =  open('surveyfileNameGoesHere.csv', 'rb')
+reader = csv.reader(surveyFile)
 
+rownum = 0
+for row in reader:
+	# save the header row.
+	if rownum == 0:
+		header = row
+	else:
+		colnum: 0
+		for colnum = 0 in row:
+			print '%-8s: %s' % (header[colnum], col)
+			colnum += 1
 
+		rownum +=1
+
+	surveyFile.close()
