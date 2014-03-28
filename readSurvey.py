4e0ea90e900fc989dc7990b1aec4d408e0e58dc8
Sharon <seaustin@live.unc.edu>
2014-03-28 10:16:59 -0400

    redid readSurvey

--- readSurvey.py
+++ readSurvey.py
@@ -1,18 +1,5 @@
 import csv
-surveyFile =  open('surveyfileNameGoesHere.csv', 'rb')
-reader = csv.reader(surveyFile)
-
-rownum = 0
-for row in reader:
-	# save the header row.
-	if rownum == 0:
-		header = row
-	else:
-		colnum: 0
-		for colnum = 0 in row:
-			print '%-8s: %s' % (header[colnum], col)
-			colnum += 1
-
-		rownum +=1
-
-	surveyFile.close()
+with open('filegoeshere.csv', 'rb') as csvfile:
+	surveyFile = csv.reader(csvfile, delimiter='	', quotechar='|')
+	for row in surveyFile:
+		print ', '.join(row)
